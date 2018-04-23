c6e97817ddc9711f5ccae9cef03be397beeffb46
Calle Lacouture <celacouture@gmail.com>
2018-04-22 22:12:15 -0500

    html and friends

---
+++ app/data/friends.js
@@ -0,0 +1,171 @@
+//array of friend data
+var friendsArray=[
+  {
+    name: 'George Clooney',
+    photo: '../assets/images/adorable-animal-black-and-white-825947.jpg',
+    scores:[
+      '1',
+      '3',
+      '2',
+      '1',
+      '5',
+      '4',
+      '4',
+      '2',
+      '1'
+    ]
+  },
+  {
+    name: 'Frank Sinatra',
+    photo: '../assets/images/adorable-animal-blur-326875.jpg',
+    score:[
+      '4',
+      '3',
+      '1',
+      '4',
+      '5',
+      '2',
+      '4',
+      '4',
+      '3',
+      '2'
+    ]
+  },
+  {
+    name: 'Beatrix Potter',
+    photo:'../assets/images/adorable-animal-bunny-255387.jpg',
+    score:[
+      '2',
+      '2',
+      '1',
+      '4',
+      '3',
+      '5',
+      '4',
+      '3',
+      '1',
+      '5'
+    ]
+  },
+  {
+    name: 'Leonardo DaVinci'
+    photo: '../assets/images/animal-animal-photography-close-up-914794.jpg'
+    score:[
+      '5',
+      '4',
+      '5',
+      '5',
+      '2',
+      '4',
+      '4',
+      '3',
+      '5',
+      '1',
+    ]
+  },
+  {
+    name: 'Dolly Parton'
+    photo:'../assets/images/animal-beak-beautiful-638738.jpg'
+    score:[
+      '4',
+      '4',
+      '1',
+      '4',
+      '3',
+      '5',
+      '2',
+      '5',
+      '3',
+      '4'
+    ]
+  },
+  {
+    name:'Hengroen'
+    photo: '../assets/images/animal-blur-horse-257573.jpg'
+    score:[
+      '4',
+      '4',
+      '4',
+      '3',
+      '2',
+      '5',
+      '1',
+      '4',
+      '1',
+      '3'
+    ]
+  },
+  {
+    name:'Ginger Rogers'
+    photo: '../assets/images/animal-cat-close-up-416195.jpg'
+    score:[
+      '2',
+      '3',
+      '1',
+      '5',
+      '4',
+      '2',
+      '5',
+      '3',
+      '4',
+      '1'
+    ]
+  },
+  {
+    name:'Benedict Cumberbatch'
+    photo:'../assets/images/animal-chameleon-close-up-584165.jpg'
+    score:[
+      '5',
+      '1',
+      '3',
+      '5',
+      '5',
+      '4',
+      '5',
+      '3',
+      '4',
+      '1'
+    ]
+  },
+  {
+    name:'Ellen DeGeneres'
+    photo:'../assets/images/aquatic-blue-colorful-60090.jpg'
+    score:[
+      '4',
+      '5',
+      '5',
+      '2',
+      '2',
+      '1',
+      '1',
+      '3',
+      '4',
+      '5'
+    ]
+  },
+  {
+    name:'Queen Elizabeth II'
+    photo: '../assets/images/animal-corgi-58997.jpg'
+    score:[
+      '2',
+      '5',
+      '2',
+      '3',
+      '4',
+      '5',
+      '4',
+      '5',
+      '4',
+      '2'
+    ]
+  }
+
+
+];
+
+
+
+
+
+
+module.exports=friendsArray;
